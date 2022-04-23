ecee6f13fe55254ff6c6791ee44e20c8fd71b227
Bichngoc1912 <bichngoc3071998@gmail.com>
2022-04-23 19:01:40 +0700

    mini fix

--- src/pages/api/queryDatabase.ts
+++ src/pages/api/queryDatabase.ts
@@ -1,9 +1,9 @@
 import { getHttpServer } from "@/helpers/httpServer";
-import { ApiResponseInterface } from "@/types/http/api";
 import { APP_CONFIGS } from "@/configs/app";
 
 const API_URL = `databases/${APP_CONFIGS.DATABASE_ID}/query`
-export async function queryDatabase(): Promise<ApiResponseInterface<any>> {
+
+export async function queryDatabase(): Promise<any> {
   const httpServer = await getHttpServer();
 
   const resp = await httpServer.post(API_URL)
