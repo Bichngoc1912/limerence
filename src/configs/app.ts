9e3e175fe32ac2d73d5025c49c34e15aaa082e35
Bichngoc1912 <bichngoc3071998@gmail.com>
2022-05-08 19:03:43 +0700

    fix 500 page

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -2,7 +2,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
 const DATABASE_ID = process.env.NEXT_PUBLIC_DATABASE_ID ?? '';
 const BEARER_TOKEN = process.env.NEXT_PUBLIC_BEARER_TOKEN ?? '';
 const NOTION_VERSION = process.env.NEXT_PUBLIC_NOTION_VERSION ?? '2022-02-22';
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'https://limerence-ntbn.vercel.app/';
 const BLUR_IMAGE_BASE64 =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0qQcAAQ0AxblT1HMAAAAASUVORK5CYII=';
 
